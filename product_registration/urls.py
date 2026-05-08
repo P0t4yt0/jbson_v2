07f885288b35d849e89c748c1dabc7b9d61e8a45
denden <dennisesinday@icloud.com>
2026-05-08 14:14:20 +0800

    Auto category prefix

--- product_registration/urls.py
+++ product_registration/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add/', views.create_product, name='create_product'),
     path('category/add-ajax/', views.add_category_ajax, name='add_category_ajax'),
     path('supplier/add-ajax/', views.add_supplier_ajax, name='add_supplier_ajax'),
+    path('ajax/categories/', views.get_categories_ajax, name='get_categories_ajax'),
 ]
