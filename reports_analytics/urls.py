4cf2ef9f549b38a5ce1916dcbaa09ab629fc8e5e
denden <dennisesinday@icloud.com>
2026-05-10 21:48:54 +0800

    supplier report

--- reports_analytics/urls.py
+++ reports_analytics/urls.py
@@ -5,4 +5,5 @@ app_name = 'reports_analytics'
 
 urlpatterns = [
     path('sales/', views.sales_report_view, name='sales_report'),
+    path('procurement/', views.procurement_report, name='procurement_report'),
 ]
