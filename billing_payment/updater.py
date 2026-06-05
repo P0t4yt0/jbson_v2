70d7c3e84196f1f7d876d37365382ff5afc8af6d
denden <dennisesinday@icloud.com>
2026-06-05 23:10:48 +0800

    billing_payment folder clean up

--- billing_payment/updater.py
+++ billing_payment/updater.py
@@ -1,5 +1,3 @@
-# jbson_v2/billing_payment/updater.py
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.core.management import call_command
 import atexit
@@ -12,10 +10,8 @@ def check_invoices_job():
         print(f"Scheduler Error: {e}")
 
 def start():
-    # Buksan ang lock file
     lock_file = open("scheduler.lock", "w")
     
-    # CROSS-PLATFORM LOCKING
     try:
         if os.name == 'nt':  
             # Para sa Windows
@@ -23,29 +19,24 @@ def start():
             try:
                 msvcrt.locking(lock_file.fileno(), msvcrt.LK_NBLCK, 1)
             except OSError:
-                return  # Naka-lock na (may tumatakbo na), wag nang mag-start ng panibago
+                return  
         else:  
             # Para sa Linux / Mac
             import fcntl
             try:
                 fcntl.lockf(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
             except BlockingIOError:
-                return  # Naka-lock na
+                return  
     except Exception as e:
         print(f"Locking error: {e}")
         return
         
     scheduler = BackgroundScheduler()
     
-    # ---------------------------------------------------------
-    # PARA SA LIVE SYSTEM: Tatakbo araw-araw tuwing 12:00 AM
     scheduler.add_job(check_invoices_job, 'cron', hour=0, minute=0)
-    # ---------------------------------------------------------
     
-    # KUNG GUSTO MONG I-TEST NGAYON: (Tanggalin ang comment sa ibaba at i-comment ang nasa itaas)
     # scheduler.add_job(check_invoices_job, 'interval', minutes=1)
 
     scheduler.start()
     
-    # Siguraduhing mamamatay ang scheduler kapag isinara ang server
     atexit.register(lambda: scheduler.shutdown(wait=False))
